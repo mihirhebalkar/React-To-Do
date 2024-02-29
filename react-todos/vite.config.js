ff7510b97d8c8153dac36c34911fdbcb3d523948
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-03-01 00:30:09 +0530

    Edited files.

--- react-todos/vite.config.js
+++ react-todos/vite.config.js
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-To-Do/",
+  base: "/React-To-Do/react-todos/",
 })
